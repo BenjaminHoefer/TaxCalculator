47b2dc313701525f7549bf374561b88b2a33388f
BenjaminHoefer <benjamin.hoefer@gmx.de>
2023-03-10 16:14:34 +0100

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,23 +16,25 @@ function calculator() {
         }
     }
 
-    /* Determine if gross tax or net tax is supposed to be calculated */
-    for (let i = 0; i < taxTypeArr.length; i++) {
-        if (taxTypeArr[i].checked) {
-            /* Calculation of gross tax */
-            if (taxTypeArr[i].value == "gross") {
-                let taxAmount = amount * taxRate;
-                document.getElementsByClassName("tax-amount")[0].innerHTML = taxAmount.toFixed(2) + " €";
-                let grossAmount = taxAmount + amount;
-                document.getElementsByClassName("end-amount")[0].innerHTML = grossAmount.toFixed(2) + " €";
-                return;
-                /* Calculation of net tax */
-            } else {
-                let netAmount = amount / (taxRate + 1);
-                document.getElementsByClassName("tax-amount")[0].innerHTML = netAmount.toFixed(2) + " €";
-                let taxAmount = amount - netAmount;
-                document.getElementsByClassName("end-amount")[0].innerHTML = taxAmount.toFixed(2) + " €";
-                return;
+    if (!isNaN(amount)) {
+        /* Determine if gross tax or net tax is supposed to be calculated */
+        for (let i = 0; i < taxTypeArr.length; i++) {
+            if (taxTypeArr[i].checked) {
+                /* Calculation of gross tax */
+                if (taxTypeArr[i].value == "gross") {
+                    let taxAmount = amount * taxRate;
+                    document.getElementsByClassName("tax-amount")[0].innerHTML = taxAmount.toFixed(2) + " €";
+                    let grossAmount = taxAmount + amount;
+                    document.getElementsByClassName("end-amount")[0].innerHTML = grossAmount.toFixed(2) + " €";
+                    return;
+                    /* Calculation of net tax */
+                } else {
+                    let netAmount = amount / (taxRate + 1);
+                    document.getElementsByClassName("tax-amount")[0].innerHTML = netAmount.toFixed(2) + " €";
+                    let taxAmount = amount - netAmount;
+                    document.getElementsByClassName("end-amount")[0].innerHTML = taxAmount.toFixed(2) + " €";
+                    return;
+                }
             }
         }
     }
